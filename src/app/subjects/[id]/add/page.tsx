ef57b1f395773ae622575c5c7763d9ae67896f49
drsushantgs <dr.sushantgsharma@icloud.com>
2025-06-13 12:33:55 +0100

    removed add

--- src/app/subjects/[id]/add/page.tsx
+++
@@ -1,5 +0,0 @@
-import AddFlashcard from "@/components/AddFlashcard";
-
-export default function AddFlashcardPage({ params }: { params: { id: string } }) {
-  return <AddFlashcard subjectUuid={params.id} />;
-}
